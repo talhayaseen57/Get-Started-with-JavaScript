26a002d97178ab5e2e8920e55423054ec1044282
talhayaseen57 <talhayaseen5725@gmail.com>
2023-01-30 13:52:09 +0100

    some variables

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // My first JavaScript code
 console.log("Hello World!");
+
+// varaibles
+let firstName = "Talha";
+let lastName = "Yaseen";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
