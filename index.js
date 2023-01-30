c7683ecfadc6e00aed1d61afdeff6d5514538a4e
talhayaseen57 <talhayaseen5725@gmail.com>
2023-01-30 13:55:18 +0100

    constants

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ let lastName = "Yaseen";
 
 let fullName = firstName + " " + lastName;
 console.log(fullName);
+
+// constants
+const intersetRate = 0.3;
+// intersetRate = 1;    cannot be reassigned bcz it's a constant
+console.log(intersetRate);
