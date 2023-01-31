c7889bf8f6e638a74c9af1ec05ef3e703049ad41
talhayaseen57 <talhayaseen5725@gmail.com>
2023-01-31 09:36:25 +0100

    privitives/value type variables

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ console.log(fullName);
 const intersetRate = 0.3;
 // intersetRate = 1;    cannot be reassigned bcz it's a constant
 console.log(intersetRate);
+
+
+// primitives/value types
+let name = "Zubair";    // string literal
+let age = 21;           // number literal
+let isApproved = false; // boolean literal
+let chawalValue = undefined;
+let selectedColor = null;   // null is used to set clear for some value
