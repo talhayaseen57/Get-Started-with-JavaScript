7cc6bbc2206cc9e160314b4bded4f09e0978cf18
talhayaseen57 <talhayaseen5725@gmail.com>
2023-01-31 10:00:07 +0100

    var, let and const

--- index.js
+++ index.js
@@ -20,3 +20,19 @@ let age = 21;           // number literal
 let isApproved = false; // boolean literal
 let chawalValue = undefined;
 let selectedColor = null;   // null is used to set clear for some value
+
+// var, let and const
+// var --> scope is within function it is declared
+// let --> scope is within block it is declared
+// const --> scope is within block but value cannot be changed once intialized
+function sayHello() {
+    // if i is declared with var then console.log(i) after for loop can access the variable i
+    // if i is declared with let then console.log(i) after for loop cannot access the variable i
+    for(let i = 0; i < 5; i++) {
+        console.log(i);
+    }
+
+    console.log(i);
+}
+
+sayHello();
