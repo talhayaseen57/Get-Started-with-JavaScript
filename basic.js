208c167f1b013006526713a5eba8073a68462645
talhayaseen57 <talhayaseen5725@gmail.com>
2024-04-02 00:42:46 +0200

    variables 1

--- basic.js
+++ basic.js
@@ -1,2 +1,3 @@
+console.log("\n\n==>basic.js<==\n\n");
 console.log("Welcome to JavaScript!");
 console.log("Talha Yaseen Zafar");
---
+++ variables.js
@@ -0,0 +1,19 @@
+console.log("\n\n==>variable.js<==\n\n");
+
+/*
+Rules for varaible names in JavaScript:
+- vaiarbles are case sensitive.
+- vaiarbles can contain only alphabets, digits, underscore (_) and $ character.
+- only alphabets, underscore (_) and $ character can be first character.
+- reserved keywords can not be a varaible name.
+- (optional) for best practice use camel-case naming convention.
+*/
+
+fulName = "Talha Yaseen Zafar";         // String
+age = 23;                               // Number
+price = 99.99;                          // Number
+x = null;                               // means there is no value, but we know it was containing some value
+y = undefined;                          // means there is no value, never initialized before
+isFollow = true;                        // Boolean
+
+console.log(isFollow);
